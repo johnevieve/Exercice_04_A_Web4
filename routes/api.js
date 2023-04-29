1c900ea021d4b591a6e5ede49131989870369dbe
MetalStrikers <129816513+MetalStrikers@users.noreply.github.com>
2023-04-28 20:47:08 -0400

    ajout controlleurs d'api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const apiController = require('../controllers/ApiController');
-router.get('/', apiController.index);
+
+router.route('/personnages')
+    .get(apiController.index)
+    .post(apiController.store)
+    .put(apiController.update)
+    .delete(apiController.delete);
+
+router.route('/personnage')
+    .get(apiController.show);
+
 module.exports = router;
