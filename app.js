1ab23dc068576a43052882bb133ee7ba897958d9
Genevieve Trudel <genevievecote10@gmail.com>
2023-05-03 22:34:26 -0400

    Petit modif

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port =  process.env.PORT;
+const port = process.env.PORT;
 const helmet = require('helmet');
 const apiRouter = require('./routes/api');
 const cors = require('cors');
 
 app.use(cors({
-    origin: ['http://localhost:3000/api/', 'http://localhost:8000']
-}));  
+    origin: 'http://localhost:8000'
+}));
 
 app.use(helmet({
     contentSecurityPolicy: false,
     referrerPolicy: { policy: 'no-referrer' }
 }));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
 app.listen(port, () => {
